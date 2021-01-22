99388fa86e3db0322dbc88c23b24f06b0e39a9b7
3530679442 <3530679442@qq.com>
2021-01-22 23:54:12 +0800

    先提交本地，摆出冲突

--- gitProject/src/com/cdc/test/TestDemo.java
+++ gitProject/src/com/cdc/test/TestDemo.java
@@ -3,7 +3,8 @@ package com.cdc.test;
 public class TestDemo {
 
 	public static void main(String[] args) {
-		//我肯定是保留我的啊
+		//我就改我就改
+		//我就改我就改
 		System.out.println("我在我的电脑上肯定是保留我的啊");
 		System.out.println("HelloWorld");
 	}
