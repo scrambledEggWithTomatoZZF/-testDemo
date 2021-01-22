03b83c81130b329837e9e49351bee0e54172e8b7
3530679442 <3530679442@qq.com>
2021-01-22 23:30:27 +0800

    解决冲突

--- gitProject/src/com/cdc/test/TestDemo.java
+++ gitProject/src/com/cdc/test/TestDemo.java
@@ -3,6 +3,8 @@ package com.cdc.test;
 public class TestDemo {
 
 	public static void main(String[] args) {
+		//我有我的style
+		System.out.println("我有我的style");
 		System.out.println("HelloWorld");
 	}
 
