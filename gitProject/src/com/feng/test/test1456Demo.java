b79354281a08546e4a88ccd4aff5ac472fda6a8c
zhangzhifeng7 <zzhif@cndatacom.com>
2021-01-22 23:17:06 +0800

    1456提交

--- gitProject/src/com/feng/test/test1456Demo.java
+++ gitProject/src/com/feng/test/test1456Demo.java
@@ -3,6 +3,10 @@
  public class test1456Demo {
 
     public static void main(String[] args) {
+        //145653修改了一些代码
+        if (true) {
+            System.out.println("true，打印成功！");
+        }
         System.out.println("我是1456532137");
 
     }
