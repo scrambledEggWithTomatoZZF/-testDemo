1c8cbfb3402232eb0e8828b3aa952badc6d0c508
zhangzhifeng7 <zzhif@cndatacom.com>
2021-01-22 23:27:37 +0800

    测试提交

--- gitProject/src/com/cdc/test/TestDemo.java
+++ gitProject/src/com/cdc/test/TestDemo.java
@@ -3,6 +3,8 @@ package com.cdc.test;
 public class TestDemo {
 
 	public static void main(String[] args) {
+	    //这是我公司电脑提交的代码
+	    System.out.println("今天好潮湿");
 		System.out.println("HelloWorld");
 	}
 
