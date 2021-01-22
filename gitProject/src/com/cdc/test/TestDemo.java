1b2ced8f62199070920693d5f25f4469a04b13fd
zhangzhifeng7 <zzhif@cndatacom.com>
2021-01-22 23:57:37 +0800

    666

--- gitProject/src/com/cdc/test/TestDemo.java
+++ gitProject/src/com/cdc/test/TestDemo.java
@@ -3,7 +3,8 @@ package com.cdc.test;
 public class TestDemo {
 
 	public static void main(String[] args) {
-		//我的怎么被修改了你再改试试?
+		//我的怎么被修改了你再改试试
+	    //看看我这里会怎么样
 		System.out.println("我在我的电脑上肯定是保留我的啊");
 		System.out.println("HelloWorld");
 	}
